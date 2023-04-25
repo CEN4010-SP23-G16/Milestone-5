0061a4de0752d8987fa89524009ff5741ac46e49
Jehad392 <116598965+Jehad392@users.noreply.github.com>
2023-04-25 18:40:47 -0400

    Add files via upload

--- src/1_WeatherPage/Info.js
+++ src/1_WeatherPage/Info.js
@@ -76,7 +76,7 @@ export default function Info() {
       </Row>
 
       <Container>
-      <Row className='forecast'>
+        <Row className='forecast'>
   {location.state.forecast.list
     .filter((data) => data.dt_txt.includes('12:00:00'))
     .slice(0, 5)
