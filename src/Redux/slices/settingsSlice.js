f3e00355077dff4ca1c750e786a7104b5c7506fb
Francisco <paquito1214.2017@gmail.com>
2023-04-29 15:58:05 -0400

    User Settings

---
+++ src/Redux/slices/settingsSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  ageRange: [
+    {id: 0, title:"18-24", checked: false },
+    {id: 1, title:"25-34", checked: false },
+    {id: 2, title:"35-44", checked: false },
+    {id: 3, title:"44-55", checked: false },
+    {id: 4, title:"55+", checked: false },
+  ],
+  allergies: '',
+  appNoti: true,
+  notificationType: {
+    extremeWeather: false,
+    forecast: false,
+    allergy: false,
+  },
+};
+
+const settingsSlice = createSlice({
+  name: 'settings',
+  initialState,
+  reducers: {
+    updateAllergies: (state, action) => {
+      state.allergies = action.payload;
+    },
+    toggleAppNoti: (state) => {
+      state.appNoti = !state.appNoti;
+    },
+    updateNotificationType: (state, action) => {
+      const { type, value } = action.payload;
+      state.notificationType[type] = value;
+    },
+     updateAgeCheckbox: (state, action) => {
+      const { id, checked } = action.payload;
+      const updatedAgeRange = state.ageRange.map(item => {
+        if (item.id === id) {
+          return { ...item, checked };
+        }
+        return item;
+      });
+      state.ageRange = updatedAgeRange;
+    },
+  },
+});
+
+export const {
+  updateAllergies,
+  toggleAppNoti,
+  updateNotificationType,
+  updateAgeCheckbox,
+} = settingsSlice.actions;
+
+export default settingsSlice.reducer;
--- src/UserSettings/UserSetting.js
+++ src/UserSettings/UserSetting.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Card, Container, Form } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import './UserSettings.css'
+import { Card, Container, Form, Button } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  updateAllergies,
+  toggleAppNoti,
+  updateNotificationType,
+  updateAgeCheckbox,
+} from '../Redux/slices/settingsSlice';
+import './UserSettings.css';
 
 export default function UserSetting() {
-  const firstName = useSelector(state => state.user.firstName);
+  const firstName = useSelector((state) => state.user.firstName);
+  const ageRange = useSelector((state) => state.settings.ageRange);
+  
+  const allergies = useSelector((state) => state.settings.allergies);
+
+  const dispatch = useDispatch();
+
+  const handleAllergiesChange = (value) => {
+    dispatch(updateAllergies(value));
+  };
+
+  const handleAppNotiToggle = (value) => {
+    dispatch(toggleAppNoti(value));
+  };
+
+  const handleNotificationTypeChange = (type, value) => {
+    dispatch(updateNotificationType({ type, value }));
+  };
+
+  const handleCheckboxChange = (name, checked) => {
+    const updatedAgeRange = ageRange.map((age) => {
+      if (age.title === name) {
+        return { ...age, checked };
+      }
+      return age;
+    });
+    console.log(updatedAgeRange);
+    dispatch(updateAgeCheckbox(updatedAgeRange));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the updated state here
+  };
 
   return (
     <>
@@ -15,87 +54,85 @@ export default function UserSetting() {
           <Card body>
             <Card.Title>User Settings</Card.Title>
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group>
-                <Form.Label>What age group best fits your needs? (Can pick multiple)</Form.Label>
+                <Form.Label>
+                  What age group best fits your needs? (Can pick multiple)
+                </Form.Label>
 
                 <div className='d-flex'>
-                  <Form.Check
-                    type='checkbox'
-                    id='18-24'
-                    label='18-24'
-                    className='m-1'
-                  />
-                  <Form.Check
-                    type='checkbox'
-                    id='25-34'
-                    label='25-34'
-                    className='m-1'
-                  />
-                  <Form.Check
-                    type='checkbox'
-                    id='35-44'
-                    label='35-44'
-                    className='m-1'
-                  />
-                  <Form.Check
-                    type='checkbox'
-                    id='44-55'
-                    label='44-55'
-                    className='m-1'
-                  />
-                  <Form.Check
-                    type='checkbox'
-                    id='55+'
-                    label='55+'
-                    className='m-1'
-                  />
+                  {ageRange.map((age, i) => (
+                    <div key={age.id}>
+                      <Form.Check
+                        type='checkbox'
+                        className='m-1'
+                        id={age.id}
+                        label={age.title}
+                        checked={age.checked}
+                        onChange={
+                          (e) => { 
+                            let check = e.target.checked;
+                            handleCheckboxChange(e.target.name, check)
+                          }
+                        }
+                        name={age.title}
+                      />
+                    </div>
+                  ))}
                 </div>
+
               </Form.Group>
 
+
               <Form.Group>
-                <Form.Label>Do you have allergies?</Form.Label>
+                <Form.Label htmlFor='allergies'>Do you have allergies?</Form.Label>
 
                 <Form.Control
                   type='text'
                   placeholder='Enter allergies (If you have any)'
                   id='allergies'
-                ></Form.Control>
+                  value={allergies}
+                  onChange={(e) => handleAllergiesChange(e.target.value)}
+                />
               </Form.Group>
 
-              <Form.Group>  
+              <Form.Group>
                 <Form.Label>Would you like to get app notifications?</Form.Label>
-
-                <Form.Select size='sm'>
-                  <option>Yes</option>
-                  <option>No</option>
+                <Form.Select size='sm' onChange={(e) => handleAppNotiToggle(e.target.value === 'Yes')}>
+                  <option value='Yes'>Yes</option>
+                  <option value='No'>No</option>
                 </Form.Select>
               </Form.Group>
 
               <Form.Group>
                 <Form.Label>Would you like to get notifications of these types?</Form.Label>
-
                 <div className='d-flex'>
                   <Form.Check
                     type='checkbox'
                     id='ExtremeWeather'
                     label='ExtremeWeather'
                     className='m-1'
+                    onChange={(e) => handleNotificationTypeChange('extremeWeather', e.target.checked)}
                   />
                   <Form.Check
                     type='checkbox'
                     id='Forecast'
                     label='Forecast'
                     className='m-1'
+                    onChange={(e) => handleNotificationTypeChange('forecast', e.target.checked)}
                   />
                   <Form.Check
                     type='checkbox'
                     id='AllergyWarnings'
                     label='AllergyWarnings'
                     className='m-1'
+                    onChange={(e) => handleNotificationTypeChange('allergy', e.target.checked)}
                   />
                 </div>
               </Form.Group>
+
+              <Button type="submit" className='mb-2'>Save Changes</Button>
+
             </Form>
             
           </Card>
